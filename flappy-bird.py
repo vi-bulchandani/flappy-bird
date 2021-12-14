589643e9835d3c10a040b0f3ca8a941bbf89cffb
Vishal Bulchandani <vi.bulchandani@gmail.com>
2021-12-14 11:02:40 +0530

    ground scroll

--- flappy-bird.py
+++ flappy-bird.py
@@ -3,19 +3,34 @@ from pygame.locals import *
 
 pygame.init()
 
+clock = pygame.time.Clock()
+
+fps = 60
+
+
 screen_width=864
 screen_height=936
 
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('FlappyBird')
 
+
+ground_scroll = 0
+scroll_speed = 4
 #images
 
 bg = pygame.image.load('img/bg.png')
+ground_img = pygame.image.load('img/ground.png')
 
 run = True
 while run:
+    clock.tick(fps)
     screen.blit(bg,(0,0))
+    screen.blit(ground_img,(ground_scroll,768))
+    ground_scroll-=scroll_speed
+
+    if (ground_scroll < -35):
+        ground_scroll=0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
